fd2ba2b8d08151f8931f47140119ec0f09f86654
yuvrajsandeepsingh-prog <yuvrajsandeepsingh@gmail.com>
2026-04-16 13:30:19 +0530

    3rd commit

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
 def add(a, b):
-    logging.info("Addition operation performed")
     return a + b
+
+if __name__ == "__main__":
+    print(add(2, 3))
